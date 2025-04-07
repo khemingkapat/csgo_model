b115755e9705f39b89e5c20b09037bcb0333b2df
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-04-07 15:42:47 +0700

    ignore .venv foler

---
+++ flake.nix
@@ -0,0 +1,96 @@
+{
+  description = "Nix-based JupyterLab with Python For CSGO Modelling";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python311;
+
+
+
+
+        pythonEnv = python.withPackages (ps: with ps; [
+          numpy
+          pandas
+          matplotlib
+          scipy
+          nbconvert
+          jupyterlab
+          ipykernel
+          plotly
+          seaborn
+          scikit-learn
+          graphviz
+          (ps.buildPythonPackage rec {
+            pname = "jupyterlab-vim";
+            version = "4.1.4";
+            pyproject = true;
+
+            src = fetchPypi {
+              pname = "jupyterlab_vim";
+              inherit version;
+              hash = "sha256-q/KJGq+zLwy5StmDIa5+vL4Mq+Uj042A1WnApQuFIlo=";
+            };
+
+            build-system = with pkgs; [
+              hatch-nodejs-version
+              hatchling
+              jupyterlab
+            ];
+
+            dependencies = with pkgs; [
+              hatch-jupyter-builder
+              jupyterlab
+            ];
+
+            pythonImportsCheck = [
+              "jupyterlab_vim"
+            ];
+          })
+          (ps.buildPythonPackage rec {
+            pname = "awpy";
+            version = "2.0.2";
+            pyproject = true;
+
+            src = fetchPypi {
+              pname = "awpy";
+              inherit version;
+              hash = "";
+            };
+
+            pythonImportsCheck = [
+              "awpy"
+            ];
+          })
+        ]);
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            jupyter
+            pandoc
+            texlive.combined.scheme-full
+          ];
+          buildInputs = [ pythonEnv pkgs.nodejs ];
+
+          shellHook = ''
+            echo "≡ƒö╣ Activating JupyterLab environment..."
+
+            # Ensure Jupyter uses the correct Python
+            export PATH="${pythonEnv}/bin:$PATH"
+
+            # Register the kernel
+            ${pythonEnv}/bin/python -m ipykernel install --user --name "nix-python" --display-name "Python (Nix)"
+
+            echo "Γ£à Jupyter Kernel Registered: Python (Nix)"
+          '';
+        };
+      });
+}
