65b9b7ed934f0344bd3bdb2149142d5c136ece49
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-05-04 15:52:53 +0700

    symbol for plotting

---
+++ utils/symbols.py
@@ -0,0 +1,10 @@
+side_color = {"ct": "Blues", "t": "Reds"}
+
+flash_marker = {"flasher": "$\u274B$", "flashee": "⌐■-■", "both": "⌐$\u274B$-■"}
+
+kill_marker = {"attacker": "o=|==>", "victim": "$\u2620$", "suicide": "$\u2622$"}
+
+grenade_marker = {
+    "thrower": "✪",
+    "victim": "☄",
+}
